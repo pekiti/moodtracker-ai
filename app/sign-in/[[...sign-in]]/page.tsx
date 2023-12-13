25ec511d29f001a5b9c077bae669be7892b6d007
Hendrik Reh <hendrik.reh@gmail.com>
2023-12-13 18:50:08 +0100

    finalize poc

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,5 @@
 import { SignIn } from '@clerk/nextjs'
 
-const SignInPage = () => {
-    return (
-        <SignIn/>
-    )
-} 
-
-export default SignInPage
+export default function SigninPage() {
+  return <SignIn signUpUrl="/sign-up" />
+}
